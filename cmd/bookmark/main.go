43651573e8d3147e31b0d90ee28bccb1e5b6ebb5
LlamaUnicorn <holidaynight@gmail.com>
2025-09-08 06:55:44 +0300

    feat: grpc runs

--- cmd/bookmark/main.go
+++ cmd/bookmark/main.go
@@ -1,12 +1,37 @@
 package main
 
-//import (
-//	"github.com/llamaunicorn/grpc_bookmarks/protobuf/api/bookmark"
-//	"google.golang.org/grpc"
-//)
+import (
+	"grpc_bookmarks/pkg/api/bookmark"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 func main() {
-	//server := grpc.NewServer()
+	server := grpc.NewServer()
 
-	//bookmark.
+	service := BookmarkService{}
+
+	bookmark.RegisterBookmarkServer(server, service)
+
+	lis, err := net.Listen("tcp", ":8082")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reflection.Register(server)
+
+	log.Println("gRPC server listening on :8082")
+
+	if err := server.Serve(lis); err != io.EOF {
+		log.Fatal(err)
+	}
+
+}
+
+type BookmarkService struct {
+	bookmark.UnimplementedBookmarkServer
 }
