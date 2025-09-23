861d668747680efa18bcf08c5750dfd37efe397d
LlamaUnicorn <holidaynight@gmail.com>
2025-09-23 06:45:44 +0300

    feat: CreateBookmarkResponse now works

--- cmd/bookmark/main.go
+++ cmd/bookmark/main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"context"
 	"grpc_bookmarks/pkg/api/bookmark"
 	"io"
 	"log"
+	"math/rand"
 	"net"
+	"sync"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
 	server := grpc.NewServer()
 
-	service := BookmarkService{}
+	service := &BookmarkService{storage: make(map[uint64]*Bookmark, 1)}
 
 	bookmark.RegisterBookmarkServer(server, service)
 
-	lis, err := net.Listen("tcp", ":8082")
+	lis, err := net.Listen("tcp", ":8085")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	reflection.Register(server)
 
-	log.Println("gRPC server listening on :8082")
+	log.Println("gRPC server listening on :8085")
 
 	if err := server.Serve(lis); err != io.EOF {
 		log.Fatal(err)
@@ -32,6 +37,36 @@ func main() {
 
 }
 
+type Bookmark struct {
+	ID    uint64
+	Title string
+	URL   string
+	Tag   string
+}
+
 type BookmarkService struct {
 	bookmark.UnimplementedBookmarkServer
+
+	storage map[uint64]*Bookmark
+	mx      sync.RWMutex
+}
+
+func (s *BookmarkService) CreateBookmark(ctx context.Context, req *bookmark.CreateBookmarkRequest) (*bookmark.CreateBookmarkResponse, error) {
+
+	id := rand.Uint64()
+	bookmarkLocal := &Bookmark{
+		ID:    id,
+		Title: req.GetTitle(),
+		URL:   req.GetUrl(),
+		Tag:   req.GetTag(),
+	}
+	s.mx.Lock()
+	s.storage[id] = bookmarkLocal
+	s.mx.Unlock()
+	return &bookmark.CreateBookmarkResponse{
+		BookmarkId: id,
+	}, nil
+}
+func (s *BookmarkService) ListBookmarks(context.Context, *bookmark.ListBookmarksRequest) (*bookmark.ListBookmarksResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListBookmarks not implemented")
 }
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Install
+
+1. go mod init github.com/username/repo
+2. mkdir -p ./protobuf/api/bookmark
+   1. create bookmark_service.proto and messages.proto
+3. touch Makefile
+4. make .bin-deps
+5. ./bin/buf config init
+6. touch buf.gen.yaml
+   either update paths and include protobuf in imports: 
+   ```
+    inputs:
+    - directory: .
+      paths:
+        - protobuf # путь до наших protobuf файлов
+   ```
+   or edit buf.yaml and import without protobuf:
+   ```
+   modules:
+   - path: protobuf
+   ```
+7. make generate
+8. mkdir -p ./cmd/bookmark
+9. touch ./cmd/bookmark/main.go
+10. start server, service and listener
+11. implement Bookmark methods Create and List in main.go
